eeb6b98744ed1b5cced138e2675d64194f83fd1a
lordace-coder <lordyacey@gmail.com>
2025-04-06 16:22:45 +0200

    t

--- flask_app/main.py
+++ flask_app/main.py
@@ -39,4 +39,5 @@ def handle_image():
     return send_from_directory(app.config["UPLOAD_FOLDER"],path="output.png")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+    port = int(os.environ.get("PORT", 10000))  # Default to 5000 if PORT is not set
+    app.run(host="0.0.0.0", port=port)
